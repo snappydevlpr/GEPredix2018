23c93b0bf414982f01e178e8a3f9b8f7fdd4e119
Austin Suarez <amsuarez@outlook.com>
2018-04-21 08:59:12 -0700

    style changes

--- ge-predix-2018/secure/scripts/populateTable.js
+++ ge-predix-2018/secure/scripts/populateTable.js
@@ -1,7 +1,9 @@
+
+
 function myFunction() {
     var row = document.getElementById("myRow");
     var x = row.insertCell(0);
 
     // insert cell
-    x.innerHTML = "New cell";
+    x.innerHTML = "";
 }
