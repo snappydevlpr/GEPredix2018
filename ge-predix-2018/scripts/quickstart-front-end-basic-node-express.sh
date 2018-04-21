8b19d140ce1d78264db66837b06c84a6e25b58fc
Austin Suarez <amsuarez@outlook.com>
2018-04-21 15:50:40 -0700

    I have no idea what im doing

---
+++ ge-predix-2018/scripts/quickstart-front-end-basic-node-express.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+set -e
+
+function local_read_args() {
+  while (( "$#" )); do
+  opt="$1"
+  case $opt in
+    -h|-\?|--\?--help)
+      PRINT_USAGE=1
+      QUICKSTART_ARGS="$SCRIPT $1"
+      break
+    ;;
+    -b|--branch)
+      BRANCH="$2"
+      QUICKSTART_ARGS+=" $1 $2"
+      shift
+    ;;
+    -o|--override)
+      QUICKSTART_ARGS=" $SCRIPT"
+    ;;
+    --skip-setup)
+      SKIP_SETUP=true
+    ;;
+    *)
+      QUICKSTART_ARGS+=" $1"
+      #echo $1
+    ;;
+  esac
+  shift
+  done
+
+  if [[ -z $BRANCH ]]; then
+    echo "Usage: $0 -b/--branch <branch> [--skip-setup]"
+    exit 1
+  fi
+}
+
+BRANCH="master"
+PRINT_USAGE=0
+SKIP_SETUP=false
+#ASSET_MODEL="-amrmd predix-ui-seed/server/sample-data/predix-asset/asset-model-metadata.json predix-ui-seed/server/sample-data/predix-asset/asset-model.json"
+SCRIPT="-script build-basic-app.sh -script-readargs build-basic-app-readargs.sh"
+QUICKSTART_ARGS="-ba -uaa -asset -ts -wd -nsts $SCRIPT"
+VERSION_JSON="version.json"
+PREDIX_SCRIPTS=predix-scripts
+REPO_NAME=predix-nodejs-starter
+SCRIPT_NAME="quickstart-front-end-webapp.sh"
+APP_DIR="build-basic-app"
+APP_NAME="Predix Front End Basic App - Node.js Express with UAA, Asset, Time Series"
+TOOLS="Cloud Foundry CLI, Git, Node.js, Maven, Predix CLI"
+TOOLS_SWITCHES="--cf --git --nodejs --maven --predixcli"
+
+local_read_args $@
+IZON_SH="https://raw.githubusercontent.com/PredixDev/izon/$BRANCH/izon.sh"
+VERSION_JSON_URL=https://raw.githubusercontent.com/PredixDev/predix-nodejs-starter/$BRANCH/version.json
+
+
+function check_internet() {
+  set +e
+  echo ""
+  echo "Checking internet connection..."
+  curl "http://google.com" > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "Unable to connect to internet, make sure you are connected to a network and check your proxy settings if behind a corporate proxy"
+    echo "If you are behind a corporate proxy, set the 'http_proxy' and 'https_proxy' environment variables.   Please read this tutorial for detailed info about setting your proxy https://www.predix.io/resources/tutorials/tutorial-details.html?tutorial_id=1565"
+    exit 1
+  fi
+  echo "OK"
+  echo ""
+  set -e
+}
+
+function init() {
+  currentDir=$(pwd)
+  if [[ $currentDir == *"scripts" ]]; then
+    echo 'Please launch the script from the root dir of the project'
+    exit 1
+  fi
+  if [[ ! $currentDir == *"$REPO_NAME" ]]; then
+    mkdir -p $APP_DIR
+    cd $APP_DIR
+  fi
+
+  check_internet
+
+
+  #get the script that reads version.json
+  eval "$(curl -s -L $IZON_SH)"
+
+  getVersionFile
+  getLocalSetupFuncs
+}
+
+if [[ $PRINT_USAGE == 1 ]]; then
+  init
+  __print_out_standard_usage
+else
+  if $SKIP_SETUP; then
+    init
+  else
+    init
+    __standard_mac_initialization
+  fi
+fi
+
+getPredixScripts
+#clone the repo itself if running from oneclick script
+getCurrentRepo
+
+echo "quickstart_args=$QUICKSTART_ARGS"
+source $PREDIX_SCRIPTS/bash/quickstart.sh $QUICKSTART_ARGS
+
+__append_new_line_log "Successfully completed $APP_NAME installation!" "$quickstartLogDir"
+__append_new_line_log "" "$quickstartLogDir"
