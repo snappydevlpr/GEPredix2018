519192b4fce33d15cf2a5a2d74434aecae45f29c
Austin Suarez <amsuarez@outlook.com>
2018-04-22 15:47:39 -0700

    4:

--- predix-nodejs-starter-master/server/app.js
+++ predix-nodejs-starter-master/server/app.js
@@ -88,10 +88,9 @@ app.get('/learning-paths', function(req, res) {
 	res.json({"learningPathsConfig": learningpaths.getLearningPaths(config)});
 });
 
-router.get("./tables",function(req,res){
+app.get("./tables",function(req,res){
   res.sendFile(path + "../tables/table.html");
 });
-
 app.use(express.static(path.join(__dirname, process.env['base-dir'] ? process.env['base-dir'] : '../public')));
 
 if (config.isUaaConfigured()) {
